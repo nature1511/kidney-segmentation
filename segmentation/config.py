6f262de5653297f8c9ae09c10fefaaa58c6c5198
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2024-01-28 17:03:05 +0300

    init  tillig

---
+++ segmentation/config.py
@@ -0,0 +1,7 @@
+class Configs:
+    # configs for tilling dataset kidney 1
+    path_img_kidney1 = 'data\\train\kidney_1_dense\\images'
+    path_lb_kidney1 = 'data\\train\kidney_1_dense\\labels'
+    tile_size = (512, 512)
+    overlap_pct = 10
+    cache_dir = 'data'
---
+++ segmentation/scr/rle_coding.py
@@ -0,0 +1,37 @@
+import numpy as np
+"""
+Code with description of rle coding.
+https://www.youtube.com/watch?v=LG8lbm_W380
+https://github.com/rohan-paul/MachineLearning-DeepLearning-Code-for-my-YouTube-Channel/blob/master/Computer_Vision/run_length_encoding.ipynb
+
+"""
+
+
+def rle_encode(img):
+    """
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    """
+    pixels = img.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return " ".join(str(x) for x in runs)
+
+
+def rle_decode(mask_rle, shape):
+    """
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return
+    Returns numpy array, 1 - mask, 0 - background
+
+    """
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int)
+                       for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape)
